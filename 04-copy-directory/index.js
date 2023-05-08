dc243186352e2ad8e877713a51af6425e31a9376
Crossroads30 <Crossroads30@mail.ru>
2023-05-08 11:25:54 +0300

    fix: fix script in04-copy-directory task (remove 'fs.cp' method)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,27 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.cp('04-copy-directory/files', '04-copy-directory/files-copy', { recursive: true }, (err) => {
+fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {//make 'files-copy' dir.
    if (err) {
       console.error(err);
+      return;
+   }
+});
+
+//read 'files' dir. to find files 
+fs.readdir('04-copy-directory/files', (err, files) => {
+   if (err) {
+      console.error(err);
+      return;
+   }
+   for (i = 0; i < files.length; i++) {
+      let allFiles = files[i];
+      //copying staff from origin to 'copy folder'
+      fs.copyFile('04-copy-directory/files/'+ allFiles, '04-copy-directory/files-copy/' + allFiles, (err) => {
+         if (err) {
+            console.error(err);
+            return;
+         }
+      })
    }
 });
