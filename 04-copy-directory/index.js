fd8637e0f3d53bf787128ee18cfcdb2c32569bf7
Crossroads30 <Crossroads30@mail.ru>
2023-05-08 23:54:08 +0300

    fix: delete comment in 03 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -33,34 +33,3 @@ fs.rm(path.join(__dirname, 'files-copy'),{
       });
    });
    });
-   
-   //read 'files' dir. to find files 
-   
-
-
-// fs.mkdir(path.join(__dirname, 'files-copy'),{//make 'files-copy' dir.
-//    recursive:true,
-// }, (err) => {
-//    if (err) {
-//       console.error(err);
-//       return;
-//    }
-// });
-
-// //read 'files' dir. to find files 
-// fs.readdir('04-copy-directory/files', (err, files) => {
-//    if (err) {
-//       console.error(err);
-//       return;
-//    }
-//    for (i = 0; i < files.length; i++) {
-//       let allFiles = files[i];
-//       //copying staff from origin to 'copy folder'
-//       fs.copyFile('04-copy-directory/files/'+ allFiles, '04-copy-directory/files-copy/' + allFiles, (err) => {
-//          if (err) {
-//             console.error(err);
-//             return;
-//          }
-//       })
-//    }
-// });
